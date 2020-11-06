d58476e318a04ca4e07a2c13c8dbb6770508a794
wynnytran <wynny.me@gmail.com>
2020-11-06 17:34:13 +0200

    build: likes counter and basic styling

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -1,7 +1,54 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-	return <h1>TASK 3</h1>;
+class App extends Component {
+	state = {
+		likes: 0,
+	};
+
+	increase = () => {
+		this.setState({
+			likes: this.state.likes + 1,
+		});
+	};
+
+	decrease = () => {
+		if (this.state.likes == 0) {
+			return;
+		}
+		this.setState({
+			likes: this.state.likes - 1,
+		});
+	};
+
+	reset = () => {
+		this.setState({
+			likes: 0,
+		});
+	};
+
+	getClasses = (e) => {};
+
+	render() {
+		const isEven = this.state.likes % 2 === 0;
+		return (
+			<div
+				style={{ backgroundColor: isEven ? 'red' : 'yellow' }}
+				className={'myClass' + (isEven ? 'even' : 'odd')}
+			>
+				<p>Total likes: {this.state.likes}</p>
+				<button id='increase-btn' onClick={this.increase}>
+					Increase
+				</button>
+				<button id='decrease-btn' onClick={this.decrease}>
+					Decrease
+				</button>
+				<button id='reset-btn' onClick={this.reset}>
+					Reset
+				</button>
+			</div>
+		);
+	}
 }
 
 export default App;
