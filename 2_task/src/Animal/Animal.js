86fb31aa51ac933714bfcc5ef310495c6b0f6d6a
wynnytran <wynny.me@gmail.com>
2020-11-06 16:44:11 +0200

    refactor: using class to create animal component

--- 2_task/src/Animal/Animal.js
+++ 2_task/src/Animal/Animal.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Animal = (props) => {
 	const showAlert = () => {
-		alert(`Hello, I am ${props.animal.name}. It is nice to meeet you!`);
+		alert(`Hello, I am ${props.name}. It is nice to meeet you!`);
 	};
 
 	return (
 		<article className='animal'>
 			<div className='img-container'>
-				<img src={props.animal.img} alt={props.animal.name} />
+				<img src={props.img} alt={props.name} />
 			</div>
-			<p>{props.animal.name}</p>
+			<p>{props.name}</p>
 			<button type='button' className='btn' onClick={showAlert}>
 				Click Me
 			</button>
--- 2_task/src/App.js
+++ 2_task/src/App.js
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Animal from './Animal/Animal';
-import { animals } from './Animal/animals';
 
-function App() {
-	const renderedAnimals = animals.map((animal) => {
-		// Spread operator, no need to have props.animal anymore
-		// return <Animal key={animal.id} {...animal}></Animal>;
-		return <Animal key={animal.id} animal={animal}></Animal>;
-	});
+class App extends Component {
+	state = {
+		animals: [
+			{
+				img:
+					'https://res.cloudinary.com/rebelwalls/image/upload/b_black,c_fill,f_auto,fl_progressive,h_533,q_auto,w_800/v1540989418/article/R11101_image1',
+				name: 'lion',
+			},
 
-	return <section className='animal-list'>{renderedAnimals}</section>;
+			{
+				img: 'https://news.gsu.edu/files/2019/10/monkey-800x600.jpg',
+				name: 'monkey',
+			},
+
+			{
+				img:
+					'https://images.newscientist.com/wp-content/uploads/2020/03/31210900/b8kk6g_web.jpg',
+				name: 'dophin',
+			},
+
+			{
+				img:
+					'https://media.npr.org/assets/img/2019/11/15/giant-panda-bei-bei-20180608-051skipbrown-_wide-5621060ac353db3650a31d9b8cb078bbbfccd9e5-s800-c85.png',
+				name: 'panda',
+			},
+		],
+	};
+	render() {
+		return (
+			<section className='animal-list'>
+				<Animal
+					img={this.state.animals[0].img}
+					name={this.state.animals[0].name}
+				></Animal>
+				<Animal
+					img={this.state.animals[1].img}
+					name={this.state.animals[1].name}
+				></Animal>
+				<Animal
+					img={this.state.animals[2].img}
+					name={this.state.animals[2].name}
+				></Animal>
+				<Animal
+					img={this.state.animals[3].img}
+					name={this.state.animals[3].name}
+				></Animal>
+			</section>
+		);
+	}
 }
 
 export default App;
